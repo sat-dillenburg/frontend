f9bf67d00a969b5c83b47baa794164061309ac3a
Silas Rosenkranz <silas.rosenkranz@student.uni-siegen.de>
2020-09-22 17:07:14 +0200

    Fix redirect bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect } from '@reach/router';
+import config from '../../gatsby-config'
 
 export default function Index(): JSX.Element {
-  return <Redirect noThrow to="/home" />;
+  return <Redirect noThrow to="home" />;
 }

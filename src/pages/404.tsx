72790d1e070590523e6c34b8ecdc9aa87402cb50
Silas Rosenkranz <silas.rosenkranz@student.uni-siegen.de>
2020-10-04 10:27:33 +0200

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
+
+import { Helmet } from 'react-helmet';
+
+export default function Index(): JSX.Element {
+  useEffect(() => {
+    navigate('/');
+  });
+
+  return (
+    <>
+      <Helmet>
+        <title>404</title>
+        <meta name="description" content="404, Seite nicht gefunden." />
+      </Helmet>
+    </>
+  );
+}
